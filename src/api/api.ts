028ee30e6aec99f0eb9e76260d7a35fc28caf1da
JOHAN-K77 <jkbrightstar2013@gmail.com>
2026-07-19 20:03:14 +0700

    Penyesuaian variable Back-End API untuk Railway Hosting

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios"
 
+const apiBaseUrl =
+  import.meta.env.VITE_API_BASE_URL ||
+  import.meta.env.VITE_API_URL ||
+  ""
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "",
+  baseURL: apiBaseUrl,
   withCredentials: false,
 })
 
