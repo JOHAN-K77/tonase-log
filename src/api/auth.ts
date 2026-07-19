34a7018d386f821d8d8e9b9ca66fef7c9fab5bef
JOHAN-K77 <jkbrightstar2013@gmail.com>
2026-07-19 20:23:13 +0700

    Kembalikan ke routing API Back-End masih pakai /api/

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,6 @@
 import api from "./api";
 
 export const loginReq = async (password: string) => {
-    const response = await api.post("/get-user", { login: password });
+    const response = await api.post("/api/get-user", { login: password });
     return response.data;
 };
